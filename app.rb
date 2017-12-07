ec6b45352c346478167b301a44bd205b7c9e4f6b
Michael Evans <michael@michaelgregoryevans.com>
2017-12-07 16:58:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    status = 200
+    "My name is Michael."
+  end
+
+  get '/hometown' do
+    status = 200
+    "My hometown is Austin."
+  end
+
+  get '/favorite-song' do
+    status = 200
+    "My favorite song is Ging Heut Morgen Ubers Feld!"
+  end
 end
