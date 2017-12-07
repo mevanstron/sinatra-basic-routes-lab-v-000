e2404b3765de6000371de44196b763c59f7f241a
Michael Evans <michael@michaelgregoryevans.com>
2017-12-07 16:54:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    resp.write "My name is Michael."
-    resp.status = 200
+    #resp.write "My name is Michael."
+    status = 200
 
   end
 end
