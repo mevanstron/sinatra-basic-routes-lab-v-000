cd3667de061c3eb51666b5451fa9414825e0c585
Michael Evans <michael@michaelgregoryevans.com>
2017-12-07 16:57:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,4 +10,9 @@ class App < Sinatra::Base
     status = 200
     "My hometown is Austin."
   end
+
+  get '/favorite-song' do
+    status = 200
+    "My favorite song is Ging Heut Morgen Ubers Feld!"
+  end
 end
